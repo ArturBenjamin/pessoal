18886e657486154e8d8765e0e1a7d9a792660545
Artur Benjamin Monteiro Nascimento <85905897+ArturBenjamin@users.noreply.github.com>
2025-04-03 21:58:07 -0300

    Update ex101.c

--- ex101.c
+++ ex101.c
@@ -3,6 +3,60 @@
 #include <getopt.h> /* get options from system argc/argv */
 #include <math.h>  /* Mathematics functions */
 
+int main(int argc, char *argv[])
+{
+    int opt; /* return from getopt() */
+
+    IFDEBUG("Starting optarg loop...");
+
+    /* getopt() configured options:
+     *        -h  help
+     *        -V  version
+     *        -v  verbose
+     */
+    opterr = 0;
+    while((opt = getopt(argc, argv, "vhV")) != EOF)
+        switch(opt)
+        {
+            case 'h':
+                help();
+                break;
+            case 'V':
+                copyr();
+                break;
+            case 'v':
+                verb++;
+                break;
+            case '?':
+            default:
+                printf("Type\n\t$man %s\nor\n\t$%s -h\nfor help.\n\n", argv[0], argv[0]);
+                return EXIT_FAILURE;
+        }
+
+    if(verb)
+        printf("Verbose level set at: %d\n", verb);
+
+   void ex10_init(); /* initialization function */
+
+    return EXIT_SUCCESS;
+}
+
+void help(void)
+{
+    IFDEBUG("help()");
+    printf("%s - %s\n", "exN", "Brief description");
+    printf("\nUsage: %s [-h|-v]\n", "exN");
+    printf("\nOptions:\n");
+    printf("\t-h,  --help\n\t\tShow this help.\n");
+    printf("\t-V,  --version\n\t\tShow version and copyright information.\n");
+    printf("\t-v,  --verbose\n\t\tSet verbose level (cumulative).\n");
+    /* add more options here */
+    printf("\nExit status:\n\t0 if ok.\n\t1 some error occurred.\n");
+    printf("\nTodo:\n\tLong options not implemented yet.\n");
+    printf("\nAuthor:\n\tWritten by %s <%s>\n\n", "Ruben Carlo Benante", "rcb@beco.cc");
+    exit(EXIT_FAILURE);
+}
+
 double bisseccao_raiz_quadrada(double x)
 {
     double li = 0; /* limite inferior */
@@ -23,6 +77,15 @@ double bisseccao_raiz_quadrada(double x)
     }
 
     return c;
+}
+
+void copyr(void)
+{
+    IFDEBUG("copyr()");
+    printf("%s - Version %s\n", "exN", VERSION);
+    printf("\nCopyright (C) %d %s <%s>, GNU GPL version 2 <http://gnu.org/licenses/gpl.html>. This  is  free  software: you are free to change and redistribute it. There is NO WARRANTY, to the extent permitted by law. USE IT AS IT IS. The author takes no responsability to any damage this software may inflige in your data.\n\n", 2016, "Ruben Carlo Benante", "rcb@beco.cc");
+    if(verb > 3) printf("copyr(): Verbose: %d\n", verb); /* -vvvv */
+    exit(EXIT_FAILURE);
 }
 
 void ex10_init(void)
