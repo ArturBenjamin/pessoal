94e545a980919cc1fa16fb2941b112f632750d22
Artur Benjamin Monteiro Nascimento <85905897+ArturBenjamin@users.noreply.github.com>
2025-04-16 16:35:25 -0300

    Create exemp1604.c

---
+++ exemp1604.c
@@ -0,0 +1,29 @@
+/* Achar o maior de 10 ints
+   Artur
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAXV 3
+
+int ,ain(void)
+{
+  int m;  // maior
+  int V[MAXV]; // vetor com valores do usuário
+  int i;  // indice
+
+/* Entrada de dados*/
+  for(i=0; i<MAXV; i++)
+    scanf("%d", &V[i])
+
+/* Processamento*/
+    m=V[0];
+    for(i=0; i<MAXV; i++)
+      if(m<V[i])
+        m=V[i];
+
+/* Saída de dados*/
+    printf("O maior valor é %d\n", m);
+    printf("O vetor é: \n");
+    ofr(i=0; i<MAXV; i++)
