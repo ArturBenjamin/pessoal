9a9bc5daa9924fbcffd8a8f87475b7ab6b4f6df7
Artur Benjamin Monteiro Nascimento <85905897+ArturBenjamin@users.noreply.github.com>
2025-04-28 15:11:32 -0300

    Create CCHO.c

---
+++ CCHO.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void)
+{
+    float GLI; /* Glicose */
+    float METGLI; /* Meta */
+    float FS; /* Fator de sensibilidade */
+    float CCHO; /* Contagem de carboidratos */
+    float RICHO; /* Relação insulina/carboidrato */
+    float QIGLI; /* Qtd de insulina para glicose */
+    float QICHO; /* Qtd de insulina para carboidrato */
+    float QIT; /* Qtd de insulina total */
+
+    printf("Glicose: \n");
+    scanf( "%d", &GLI);
+    printf("Meta: \n");
+    scanf( "%d", &METGLI);
+    printf("Fator de sensibilidade: \n");
+    scanf( "%d", &FS);
+    printf("Quantidade de CHO: \n");
+    scanf( "%d", &CCHO);
+    printf("Relação I/R: \n");
+    scanf( "%d", &RICHO);
+
+    if(GLI <= METGLI)
+    {
+        if(GLI < 80)
+        {
+            QIGLI = (-1);
+        }
+        else(GLI >= 80)
+        {
+            QIGLI = 0;    
+        }
+    }
+    else(GLI > METGLI)
+    {
+        QIGLI = (GLI - 80)/FS;
+    }
+    QICHO = CCHO/RICHO;
+
+    QIT = QIGLI + QICHO;
+
+    printf("A quantidade de insulina em unidades será: %.2f", QIT);
+
+    return 0;
+}
