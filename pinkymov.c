517dd426e3d73579e2966fef90a156ecada2abee
Artur Benjamin Monteiro Nascimento <85905897+ArturBenjamin@users.noreply.github.com>
2025-06-18 12:47:53 -0300

    Update pinkymov.c

--- pinkymov.c
+++ pinkymov.c
@@ -1,10 +1,11 @@
 t_game pinkymove(t_game g, int elapsed_seconds)
 {
-    if (elapsed_seconds < g.ghost[pinky].start_time) return g;
+    int i = 1; // Índice do Pinky
+    if (elapsed_seconds < g.ghost[i].start_time) return g;
 
     t_pos target; // Onde o Pinky quer chegar
-    int y = g.ghost[pinky].pos.y;
-    int x = g.ghost[pinky].pos.x;
+    int y = g.ghost[i].pos.y;
+    int x = g.ghost[i].pos.x;
 
     // =================================================================================
     // PASSO 1: CALCULAR O ALVO
@@ -37,44 +38,44 @@ t_game pinkymove(t_game g, int elapsed_seconds)
     // =================================================================================
     long int dist_up = 99999, dist_down = 99999, dist_left = 99999, dist_right = 99999;
 
-    if (y > 0 && g.ghost[pinky].dir != down && g.lab[y - 1][x] != '#')
-        dist_up = pow(y - 1 - target.y, 2) + pow(x - target.x, 2);
+    if (y > 0 && g.ghost[i].dir != down && g.lab[y - 1][x] != '#')
+        dist_up = (y - 1 - target.y)*(y - 1 - target.y) + (x - target.x)*(x - target.x);
 
-    if (y < 22 && g.ghost[pinky].dir != up && g.lab[y + 1][x] != '#')
-        dist_down = pow(y + 1 - target.y, 2) + pow(x - target.x, 2);
+    if (y < 22 && g.ghost[i].dir != up && g.lab[y + 1][x] != '#')
+        dist_down = (y + 1 - target.y)*(y + 1 - target.y) + (x - target.x)*(x - target.x);
 
-    if (x > 0 && g.ghost[pinky].dir != right && g.lab[y][x - 1] != '#')
-        dist_left = pow(y - target.y, 2) + pow(x - 1 - target.x, 2);
+    if (x > 0 && g.ghost[i].dir != right && g.lab[y][x - 1] != '#')
+        dist_left = (y - target.y)*(y - target.y) + (x - 1 - target.x)*(x - 1 - target.x);
 
-    if (x < 19 && g.ghost[pinky].dir != left && g.lab[y][x + 1] != '#')
-        dist_right = pow(y - target.y, 2) + pow(x + 1 - target.x, 2);
+    if (x < 19 && g.ghost[i].dir != left && g.lab[y][x + 1] != '#')
+        dist_right = (y - target.y)*(y - target.y) + (x + 1 - target.x)*(x + 1 - target.x);
 
     if (dist_up <= dist_down && dist_up <= dist_left && dist_up <= dist_right)
-        g.ghost[pinky].dir = up;
+        g.ghost[i].dir = up;
     else if (dist_down <= dist_up && dist_down <= dist_left && dist_down <= dist_right)
-        g.ghost[pinky].dir = down;
+        g.ghost[i].dir = down;
     else if (dist_left <= dist_up && dist_left <= dist_down && dist_left <= dist_right)
-        g.ghost[pinky].dir = left;
+        g.ghost[i].dir = left;
     else
-        g.ghost[pinky].dir = right;
+        g.ghost[i].dir = right;
 
     // =================================================================================
     // PASSO 3: ATUALIZAR A POSIÇÃO
     // =================================================================================
     int prev_y = y, prev_x = x;
 
-    if(g.ghost[pinky].dir == up) y--;
-    else if(g.ghost[pinky].dir == down) y++;
-    else if(g.ghost[pinky].dir == left) x--;
-    else if(g.ghost[pinky].dir == right) x++;
+    if(g.ghost[i].dir == up) y--;
+    else if(g.ghost[i].dir == down) y++;
+    else if(g.ghost[i].dir == left) x--;
+    else if(g.ghost[i].dir == right) x++;
 
     // Lógica do túnel
     if (y == 10 && x <= 0) x = 17;
     else if (y == 10 && x >= 18) x = 1;
 
     g.lab[prev_y][prev_x] = ' ';
-    g.ghost[pinky].pos.y = y;
-    g.ghost[pinky].pos.x = x;
+    g.ghost[i].pos.y = y;
+    g.ghost[i].pos.x = x;
     g.lab[y][x] = 'P';
 
     return g;
