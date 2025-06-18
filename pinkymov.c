46a468095474fa3e5ef137a6c7c9e63b8f0a62c9
Artur Benjamin Monteiro Nascimento <85905897+ArturBenjamin@users.noreply.github.com>
2025-06-18 12:34:34 -0300

    Update pinkymov.c

--- pinkymov.c
+++ pinkymov.c
@@ -1,6 +1,10 @@
 t_game pinkymove(t_game g, int elapsed_seconds)
 {
+    if (elapsed_seconds < g.ghost[pinky].start_time) return g;
+
     t_pos target; // Onde o Pinky quer chegar
+    int y = g.ghost[pinky].pos.y;
+    int x = g.ghost[pinky].pos.x;
 
     // =================================================================================
     // PASSO 1: CALCULAR O ALVO
@@ -9,7 +13,7 @@ t_game pinkymove(t_game g, int elapsed_seconds)
     {
         case up:
             target.y = g.pacman.pos.y - 4;
-            target.x = g.pacman.pos.x - 4; // Peculiaridade do jogo original
+            target.x = g.pacman.pos.x - 4; // Peculiaridade do Pinky original
             break;
         case down:
             target.y = g.pacman.pos.y + 4;
@@ -23,10 +27,8 @@ t_game pinkymove(t_game g, int elapsed_seconds)
             target.y = g.pacman.pos.y;
             target.x = g.pacman.pos.x + 4;
             break;
-        // Caso o Pac-Man esteja parado, o Pinky pode mirar diretamente nele
         default:
-            target.y = g.pacman.pos.y;
-            target.x = g.pacman.pos.x;
+            target = g.pacman.pos;
             break;
     }
 
@@ -35,31 +37,18 @@ t_game pinkymove(t_game g, int elapsed_seconds)
     // =================================================================================
     long int dist_up = 99999, dist_down = 99999, dist_left = 99999, dist_right = 99999;
 
-    // Calcula a distância para o alvo se for para CIMA
-    if (g.ghost[pinky].dir != down && g.lab[g.ghost[pinky].pos.y - 1][g.ghost[pinky].pos.x] != '#')
-    {
-        dist_up = pow(g.ghost[pinky].pos.y - 1 - target.y, 2) + pow(g.ghost[pinky].pos.x - target.x, 2);
-    }
+    if (y > 0 && g.ghost[pinky].dir != down && g.lab[y - 1][x] != '#')
+        dist_up = pow(y - 1 - target.y, 2) + pow(x - target.x, 2);
 
-    // Calcula a distância para o alvo se for para BAIXO
-    if (g.ghost[pinky].dir != up && g.lab[g.ghost[pinky].pos.y + 1][g.ghost[pinky].pos.x] != '#')
-    {
-        dist_down = pow(g.ghost[pinky].pos.y + 1 - target.y, 2) + pow(g.ghost[pinky].pos.x - target.x, 2);
-    }
+    if (y < 22 && g.ghost[pinky].dir != up && g.lab[y + 1][x] != '#')
+        dist_down = pow(y + 1 - target.y, 2) + pow(x - target.x, 2);
 
-    // Calcula a distância para o alvo se for para ESQUERDA
-    if (g.ghost[pinky].dir != right && g.lab[g.ghost[pinky].pos.y][g.ghost[pinky].pos.x - 1] != '#')
-    {
-        dist_left = pow(g.ghost[pinky].pos.y - target.y, 2) + pow(g.ghost[pinky].pos.x - 1 - target.x, 2);
-    }
+    if (x > 0 && g.ghost[pinky].dir != right && g.lab[y][x - 1] != '#')
+        dist_left = pow(y - target.y, 2) + pow(x - 1 - target.x, 2);
 
-    // Calcula a distância para o alvo se for para DIREITA
-    if (g.ghost[pinky].dir != left && g.lab[g.ghost[pinky].pos.y][g.ghost[pinky].pos.x + 1] != '#')
-    {
-        dist_right = pow(g.ghost[pinky].pos.y - target.y, 2) + pow(g.ghost[pinky].pos.x + 1 - target.x, 2);
-    }
+    if (x < 19 && g.ghost[pinky].dir != left && g.lab[y][x + 1] != '#')
+        dist_right = pow(y - target.y, 2) + pow(x + 1 - target.x, 2);
 
-    // Compara as distâncias e escolhe o menor caminho
     if (dist_up <= dist_down && dist_up <= dist_left && dist_up <= dist_right)
         g.ghost[pinky].dir = up;
     else if (dist_down <= dist_up && dist_down <= dist_left && dist_down <= dist_right)
@@ -72,19 +61,21 @@ t_game pinkymove(t_game g, int elapsed_seconds)
     // =================================================================================
     // PASSO 3: ATUALIZAR A POSIÇÃO
     // =================================================================================
-    if(g.ghost[pinky].dir == up) g.ghost[pinky].pos.y--;
-    else if(g.ghost[pinky].dir == down) g.ghost[pinky].pos.y++;
-    else if(g.ghost[pinky].dir == left) g.ghost[pinky].pos.x--;
-    else if(g.ghost[pinky].dir == right) g.ghost[pinky].pos.x++;
+    int prev_y = y, prev_x = x;
+
+    if(g.ghost[pinky].dir == up) y--;
+    else if(g.ghost[pinky].dir == down) y++;
+    else if(g.ghost[pinky].dir == left) x--;
+    else if(g.ghost[pinky].dir == right) x++;
 
     // Lógica do túnel
-    if (g.ghost[pinky].pos.y == 10) {
-        if (g.ghost[pinky].pos.x <= 0) {
-            g.ghost[pinky].pos.x = 17;
-        } else if (g.ghost[pinky].pos.x >= 18) {
-            g.ghost[pinky].pos.x = 1;
-        }
-    }
+    if (y == 10 && x <= 0) x = 17;
+    else if (y == 10 && x >= 18) x = 1;
+
+    g.lab[prev_y][prev_x] = ' ';
+    g.ghost[pinky].pos.y = y;
+    g.ghost[pinky].pos.x = x;
+    g.lab[y][x] = 'P';
 
     return g;
 }
