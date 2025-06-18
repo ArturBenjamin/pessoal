cb074d907eb973d4356557d73cd5050e2be3ed88
Artur Benjamin Monteiro Nascimento <85905897+ArturBenjamin@users.noreply.github.com>
2025-06-18 16:55:20 -0300

    Update pinkymodos.c

--- pinkymodos.c
+++ pinkymodos.c
@@ -1,62 +1,75 @@
-#include "upecman.h"
-#include <stdlib.h>
+t_game modosfantasmas(t_game g)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        switch (g.ghost[i].mode)
+        {
+            case chase:
+            if (i == 1)
+            {
+                // Pinky
+                g.ghost[i].starget = g.pacman.pos;
+                switch (g.pacman.dir)
+                {
+                    case up:
+                    g.ghost[i].starget.y -= 4;
+                    g.ghost[i].starget.x -= 4;
+                    break;
+                    case down:
+                    g.ghost[i].starget.y += 4;
+                    break;
+                    case left:
+                    g.ghost[i].starget.x -= 4;
+                    break;
+                    case right:
+                    g.ghost[i].starget.x += 4;
+                    break;
+                    default:
+                    break;
+                }
+            }
+            break;
 
-// MODO CHASE — Pinky mira 4 blocos à frente do Pac-Man
+            case scatter:
+            if (i == 1)
+            {
+                // Pinky
+                g.ghost[i].starget.y = 0;
+                g.ghost[i].starget.x = 0;
+            }
+            break;
 
-t_pos pinky_chase(t_game g) {
-    t_pos target = g.pacman.pos;
-    switch (g.pacman.dir) {
-        case up:
-            target.y -= 4;
-            target.x -= 4; // Comportamento clássico do jogo original
+            case frightened:
+            if (i == 1)
+            {
+                // Pinky
+                g.ghost[i].starget.y = rand() % 23;
+                g.ghost[i].starget.x = rand() % 20;
+            }
             break;
-        case down:
-            target.y += 4;
+
+            case afraud:
+            if (i == 1)
+            {
+                // Pinky
+                g.ghost[i].starget.y = 22;
+                g.ghost[i].starget.x = 18;
+            }
             break;
-        case left:
-            target.x -= 4;
+
+            case dead:
+            if (i == 1)
+            {
+                // Pinky
+                g.ghost[i].starget.y = 10;
+                g.ghost[i].starget.x = 10;
+            }
             break;
-        case right:
-            target.x += 4;
+
+            default:
             break;
-        default:
-            break;
+        }
     }
-    return target;
-}
 
-// MODO SCATTER — Pinky foge para o canto superior esquerdo
-
-t_pos pinky_scatter(t_game g) {
-    t_pos target;
-    target.y = 0;
-    target.x = 0;
-    return target;
-}
-
-// MODO FRIGHTENED — Pinky foge para um ponto aleatório
-
-t_pos pinky_frightened(t_game g) {
-    t_pos target;
-    target.y = rand() % 23;
-    target.x = rand() % 20;
-    return target;
-}
-
-// MODO AFRAUD — Pinky foge para o canto inferior direito
-
-t_pos pinky_afraud(t_game g) {
-    t_pos target;
-    target.y = 22;
-    target.x = 18;
-    return target;
-}
-
-// MODO DEAD — Pinky retorna para o centro da casa dos fantasmas
-
-t_pos pinky_dead(t_game g) {
-    t_pos target;
-    target.y = 10;
-    target.x = 10;
-    return target;
+return g;
 }
