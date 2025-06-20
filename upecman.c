dcbac0fe5bd4af80f753cb9aa5265e954c48de99
Artur Benjamin Monteiro Nascimento <abmn@poli.br>
2025-06-20 01:04:17 -0300

    -a

--- upecman.c
+++
@@ -1,3 +0,0 @@
-// Arquivo upecman.c
-
-// Aqui ficará o código final com movimentação e modos dos fantasmas.
--- upecman.h
+++
@@ -1,3 +0,0 @@
-// Arquivo upecman.h
-
-// Aqui ficará o cabeçalho atualizado com as funções dos fantasmas.
